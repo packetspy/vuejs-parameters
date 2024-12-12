c133ad247d60ea658feafe8786a339f6cdff8c43
Anderson Tomazeto <packetspy@gmail.com>
2024-12-12 17:10:37 -0300

    teste with css+img

--- src/views/PageWithParameter.vue
+++ src/views/PageWithParameter.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
     <h1>Page with parameter</h1>
-    <p>Unique ID: {{ uniqueId }}</p>
+    <p class="anyway">Unique ID: {{ uniqueId }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
+import '@/style.css'
+import '@/assets/css/index.css'
 import { router } from '@/routes'
 const uniqueId: string | string[] = router.currentRoute.value.params.uniqueId
 </script>
